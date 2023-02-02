3a2167b705a73e45a4319cecc97d10e3d4f275f4
pattonliu84 <113130211+pattonliu84@users.noreply.github.com>
2023-02-02 11:28:44 -0800

    Update Connect4.java

--- src/main/java/u5pp1/Connect4.java
+++ src/main/java/u5pp1/Connect4.java
@@ -2,17 +2,114 @@ package u5pp1;
 
 public class Connect4 {
 
-    public static final int RED_WIN = 0;
-    public static final int BLACK_WIN = 1;
-    public static final int NO_WINNER = 2;
-    public static final int BOTH_WIN = 3;
+  public static final int RED_WIN = 0;
+  public static final int BLACK_WIN = 1;
+  public static final int NO_WINNER = 2;
+  public static final int BOTH_WIN = 3;
 
-    public static final int RED = 1;
-    public static final int BLACK = -1;
-    public static final int EMPTY = 0;
+  public static final int RED = 1;
+  public static final int BLACK = -1;
+  public static final int EMPTY = 0;
 
-   // implementation here
+  public static boolean isFull(int[][] board) {
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == EMPTY) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
 
-   // It is recommended to use private helper methods
+  public static boolean isBoardValid(int[][] board) {
+    for (int row = 0; row < board.length; row++) {
+      for (int col = 0; col < board[row].length; col++) {
+        if (board[row][col] == 1 || board[row][col] == -1) {
+          for (int rowJ = row + 1; rowJ < board.length; rowJ++) {
+            if (board[rowJ][col] == 0) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
 
+  public static int getWinner(int[][] board) {
+    boolean REDWIN = false;
+    boolean BLACKWIN = false;
+
+    //Checks horizontally
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length - 3; j++) {
+        if (board[i][j] == 1 && board[i][j + 1] == 1 && board[i][j + 2] == 1 && board[i][j + 3] == 1) {
+              REDWIN = true;
+            } else if (board[i][j] == -1 && board[i][j + 1] == -1 && board[i][j + 2] == -1 && board[i][j+3] == -1) {
+              BLACKWIN = true;
+            }
+          }
+        }
+      
+//Checks vertically
+    for (int i = 0; i < board.length - 3; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        if (board[i][j] == 1 && board[i + 1][j] == 1 && board[i + 2][j] == 1 && board[i + 3][j] == 1) {
+          REDWIN = true;
+        } else if (board[i][j] == -1 && board[i + 1][j] == -1 && board[i + 2][j] == -1 && board[i + 3][j] == -1) {
+          BLACKWIN = true;
+        }
+      }
+    }
+
+    //Checks diagonally
+    for (int i = 3; i < board.length; i++) {
+    for(int j = 0; j<board[i].length - 3; j++){
+        if(board[i][j] == 1){
+        if(board[i-1][j+1] == 1){
+                if((board[i-2][j+2] == 1) && (board[i-3][j+3] == 1)){
+                    REDWIN = true;
+                }
+            }
+                } else if(board[i][j] == -1){
+                    if(board[i-1][j+1] == -1){
+                    if((board[i-2][j+2] == -1) && (board[i-3][j+3] == -1)){
+                    BLACKWIN = true;
+                }
+            }
+        }
+    }
+}
+
+    for(int i = 0; i<board.length-3; i++){
+        for(int j = 0; j<board[0].length - 3; j++){
+            if(board[i][j] == 1){
+                if(board[i+1][j+1] == 1){
+                    if((board[i+2][j+2] == 1) && (board[i+3][j+3] == 1)){
+                    REDWIN = true;
+                }
+            }
+                } else if(board[i][j] == -1){
+                    if(board[i+1][j+1] == -1){
+                        if((board[i+2][j+2] == -1) && (board[i+3][j+3] == -1)){
+                        BLACKWIN = true;
+                    }
+                }
+            }
+        }
+    }
+    
+    if(REDWIN && BLACKWIN){
+        return BOTH_WIN;
+    } else if(REDWIN){
+        return RED_WIN;
+    } else if(BLACKWIN){
+        return BLACK_WIN;
+    } else{
+        return NO_WINNER;
+    }
+    
+    }
+    
 }
